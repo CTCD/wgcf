a68a7f754ca19ec2c97d050e15590065890d4922
CTCD <17850936+CTCD@users.noreply.github.com>
2023-07-23 22:21:17 +0800

    Update yxip.sh

--- yxip.sh
+++ yxip.sh
@@ -38,7 +38,7 @@ while [ $(wc -l <ip.txt) -lt 200 ]; do
     fi
 done
 
-wget https://raw.githubusercontent.com/CTCD/wgcf/main/warp-yxip-linux-$(upc_type) -O yxip
+[ ! -f yxip ] && wget https://raw.githubusercontent.com/CTCD/wgcf/main/warp-yxip-linux-$(upc_type) -O yxip
 chmod +x yxip && ./yxip
 cat result.csv | head -31
 rm -f ip.txt result.csv
