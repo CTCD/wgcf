516f9f7c534ed98498345cd55488c6427fb0bdf0
CTCD <17850936+CTCD@users.noreply.github.com>
2023-07-25 23:29:35 +0800

    Update yxip.sh

--- yxip.sh
+++ yxip.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 upc_type() {
     case "$(uname -m)" in
     armv8 | arm64 | aarch64) echo 'arm64' ;;
@@ -8,7 +6,6 @@ upc_type() {
     *) red "CPU architecture not supported" && exit 0 ;;
     esac
 }
-
 generate_random_ipv4() {
     echo "162.159.192.$((RANDOM % 255))"
     echo "162.159.193.$((RANDOM % 255))"
@@ -19,7 +16,6 @@ generate_random_ipv4() {
     echo "188.114.98.$((RANDOM % 255))"
     echo "188.114.99.$((RANDOM % 255))"
 }
-
 generate_random_ipv6() {
     printf '2606:4700:d%d::%x:%x:%x:%x' $((RANDOM % 2)) $((RANDOM * RANDOM % 65536)) $((RANDOM * RANDOM % 65536)) $((RANDOM * RANDOM % 65536)) $((RANDOM * RANDOM % 65536))
 }
@@ -31,7 +27,6 @@ while [ $(wc -l <ip.txt) -lt 100 ]; do
         echo "$address" >>ip.txt
     fi
 done
-
 while [ $(wc -l <ip.txt) -lt 200 ]; do
     address=$(generate_random_ipv6)
     if ! grep -q "$address" ip.txt; then
@@ -39,7 +34,7 @@ while [ $(wc -l <ip.txt) -lt 200 ]; do
     fi
 done
 
-[ ! -f yxip ] && wget https://raw.githubusercontent.com/CTCD/wgcf/main/warp-yxip-linux-$(upc_type) -O yxip
-chmod +x yxip && ./yxip
+[ ! -f warp-linux-$(upc_type) ] && wget https://raw.githubusercontent.com/CTCD/wgcf/main/warp-linux-$(upc_type)
+chmod +x warp-linux-$(upc_type) && ./warp-linux-$(upc_type)
 cat result.csv | awk -F',' '$3 != "timeout ms" {print}' | head -30
 rm -f ip.txt result.csv
